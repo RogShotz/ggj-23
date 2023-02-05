0745ab5caedde167338465a1682c8c9a78a34b0a
JefferNg <jefferng11@gmail.com>
2023-02-05 15:18:50 -0800

    created lives counter

---
+++ Assets/Scripts/TextManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextManager : MonoBehaviour
+{
+    private TreeBehavior tree;
+    public TextMeshProUGUI livesRemaining;
+
+    void Awake()
+    {
+        tree = GameObject.Find("TreeSprite").GetComponent<TreeBehavior>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        livesRemaining.text = "Lives: " + tree.getLives().ToString();
+    }
+}
---
+++ Assets/Scripts/TreeBehavior.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBehavior : MonoBehaviour
+{
+    public static int lives = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Debug.Log("Tree hit");
+            lives--;
+        }
+    }
+
+    public int getLives()
+    {
+        return lives;
+    }
+
+    public void setLives(int val)
+    {
+        lives = val;
+    }
+
+}
