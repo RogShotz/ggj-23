05cf071c6d4557495f976bdb5818ee76a0cae251
JefferNg <jefferng11@gmail.com>
2023-02-05 10:46:47 -0800

    moved scripts to folder, added red gem

--- Assets/Scripts/AABehavior.cs
+++ Assets/Scripts/AABehavior.cs
@@ -6,23 +6,34 @@ public class AABehavior : MonoBehaviour
 {
     public ProjectileBehavior projectile;
     public Vector3 launchOffset;
+    private float delay = 1f;
+    private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Shoot", 2f, 1f);
+        InvokeRepeating("Shoot", 2f, 3f);
         launchOffset = new Vector3(transform.position.x + 0.4f, transform.position.y, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
     }
 
     void Shoot()
     {
-        Instantiate(projectile, launchOffset, transform.rotation);
+        for (int i = 0; i < 3; i++)
+        {
+            if (timer > delay)
+            {
+                timer = 0;
+                Instantiate(projectile, launchOffset, transform.rotation);
+            }
+            
+        }
+
     }
 
 }
