82f46b7c1c1d55572efaa429b933f853c976d6aa
JefferNg <jefferng11@gmail.com>
2023-02-05 12:02:36 -0800

    AA tower shoots in bursts

--- Assets/Scripts/AABehavior.cs
+++ Assets/Scripts/AABehavior.cs
@@ -6,13 +6,15 @@ public class AABehavior : MonoBehaviour
 {
     public ProjectileBehavior projectile;
     public Vector3 launchOffset;
-    private float delay = 1f;
-    private float timer;
+    private float delay = 5f;
+    private int burstShots = 3;
+    private float timer = 0f;
+    bool shoot = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Shoot", 2f, 3f);
+        InvokeRepeating("Shoot", 2f, 0.3f);
         launchOffset = new Vector3(transform.position.x + 0.4f, transform.position.y, transform.position.z);
     }
 
@@ -20,18 +22,24 @@ public class AABehavior : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        if (timer >= delay && burstShots == 3)
+        {
+            shoot = true;
+            timer = 0f;
+        }
+        else if (burstShots < 1)
+        {
+            burstShots = 3;
+            shoot = false;
+        }
     }
 
     void Shoot()
     {
-        for (int i = 0; i < 3; i++)
-        {
-            if (timer > delay)
-            {
-                timer = 0;
-                Instantiate(projectile, launchOffset, transform.rotation);
-            }
-            
+        if (shoot)
+        {       
+            Instantiate(projectile, launchOffset, transform.rotation);
+            burstShots--;
         }
 
     }
