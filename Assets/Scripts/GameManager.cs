282ba393cf3907429d6ac93bc602279ef2b2b769
JefferNg <jefferng11@gmail.com>
2023-02-05 17:06:43 -0800

    random gem generation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GemBehavior gem;
+    public GemBehavior redGem;
+    public GemBehavior greenGem;
+    public GemBehavior blueGem;
     public float timer = 0f;
     public float delay = 3f;
     public Vector3 launchOffset;
+    public int randomGem = Random.Range(0, 3);
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +23,19 @@ public class GameManager : MonoBehaviour
         timer += Time.deltaTime;
         if (timer > delay)
         {
-            Instantiate(gem, launchOffset, transform.rotation);
+            if (randomGem == 0)
+            {
+                Instantiate(greenGem, launchOffset, transform.rotation);
+            }
+            else if (randomGem == 1)
+            {
+                Instantiate(blueGem, launchOffset, transform.rotation);
+            }
+            else
+            {
+                Instantiate(redGem, launchOffset, transform.rotation);
+            }
+            randomGem = Random.Range(0, 3);
             timer = 0f;
             delay = Random.Range(1, 10);
             launchOffset = new Vector3(transform.position.x + Random.Range(0,2), transform.position.y, transform.position.z);
